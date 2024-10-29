8c811c5a4e74d47bea1833bc8b2bddb88296b814
akanksha <akankshabhagat606@gmail.com>
2024-10-29 17:32:48 +0530

    changes and learning

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 this_dir = pathlib.Path(__file__).resolve().parent
 def home_page_view(request, *args,**kwargs):
-   html_=""
    html_file_path=this_dir/"home.html"
    html_=html_file_path.read_text()
    return HttpResponse(html_)
