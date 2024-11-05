68e31d8abe32e2a85c5787d71fbf5084c495c5cc
akanksha <akankshabhagat606@gmail.com>
2024-11-05 19:41:34 +0530

    changes acccording to update
    the templates

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', 
+    #my apps
+   ' visits',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,10 @@ ROOT_URLCONF = 'cfehome.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/"templates",
+         
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,11 +1,52 @@
 import pathlib
+from django.shortcuts import render
 from django.http import HttpResponse
 
 this_dir = pathlib.Path(__file__).resolve().parent
+
+
+
+
+
+
 def home_page_view(request, *args,**kwargs):
-   html_file_path=this_dir/"home.html"
-   html_=html_file_path.read_text()
-   return HttpResponse(html_)
+   my_title="Home page"
+   my_content={
+      "page_title":my_title,
+         
+            }
+   html_template="home.html"
+   return render(request,html_template,my_content)
+
+
+
+
+
+
+# --------------------------------------------------------- OLD HOME PAGE TRAIL  TEMPLATE COPY ------------------------------------------------------------------
+# def old_home_page_view(request, *args,**kwargs):
+#    my_title="Homepage"
+#    my_content={
+#       "page_title":my_title,
+         
+#             }
+#    html_="""
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <title>Document</title>
+# </head>
+# <body>
+#     <h1>{page_title}</h1>
+# </body>
+# </html>
+
+# """.format(**my_content)
+# #    html_file_path=this_dir/"home.html"
+# #    html_=html_file_path.read_text()
+#    return HttpResponse(html_)
  
 
   
