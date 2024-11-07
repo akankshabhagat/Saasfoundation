54d1258f67ecbb4df081771999aef2a9b0986148
akanksha <akankshabhagat606@gmail.com>
2024-11-07 21:23:36 +0530

    submitting files
    ading files to railway for production deployment

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -9,21 +9,8 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 
 
-def home_page_view(request, *args,**kwargs):
-   qs=PageVisit.objects.all()
-   page_qs=PageVisit.objects.filter(path=request.path)
-
-   my_title="Hello akanksha its working"
-   my_content={
-      "page_title":my_title,
-      "pagevisit_count":page_qs.count(),
-      "total_visit_count":qs.count(),
-            }
-  
-   html_template="home.html"
-   PageVisit.objects.create(path=request.path)
-   return render(request,html_template,my_content)
-
+def home_view(request, *args,**kwargs):
+   return about_view(request,*args,**kwargs)
 
 
 def about_view(request,*args,**kwargs):
