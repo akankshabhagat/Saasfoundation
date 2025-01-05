32507bb887ba4aad6873ddc1ec6acf83fecee093
akanksha <akankshabhagat606@gmail.com>
2025-01-05 19:24:16 +0530

    restarting learning and updating

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -11,16 +11,20 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_view(request, *args,**kwargs):
    return about_view(request,*args,**kwargs)
-
+    
 
 def about_view(request,*args,**kwargs):
    qs=PageVisit.objects.all()
    page_qs=PageVisit.objects.filter(path=request.path)
-
+   try:
+      percent = (page_qs.count() * 100.0)/ qs.count(),
+   except:
+      percent = 0
    my_title="Hello akanksha its working"
    my_content={
       "page_title":my_title,
       "pagevisit_count":page_qs.count(),
+      "percent":percent,
       "total_visit_count":qs.count(),
             }
   
